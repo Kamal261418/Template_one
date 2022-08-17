1e8fc02537fbcda8b4c3d983abb77c8e9761e512
kamal elfiky <elfikykamal@gmail.com>
2022-08-17 22:17:32 +0200

    create project html css bootstrap

---
+++ js/main.js
@@ -0,0 +1,74 @@
+
+// start AllBox
+// $(".colorBox li").eq(0).css("background-color","blue");
+// $(".colorBox li").eq(1).css("background-color","green");
+// $(".colorBox li").eq(2).css("background-color","yellow");
+// $(".colorBox li").eq(3).css("background-color","salmon");
+// $(".colorBox li").eq(4).css("background-color","teal");
+
+
+
+
+// $(".colorBox li").click(function(){
+
+
+//     let getColor= $(this).css("background-color");
+//     $("").css("color", getColo);
+// })
+
+// //
+
+// let colorWidth=$(".colorBox").outerWidth();
+
+// $(".AllBox i").click(function(){
+
+
+//     let AllBoxLeft = $(".AllBox").css("left");
+//     if(AllBoxLeft=="0px")
+//     {
+//         $(".AllBox").animate({left:`-${colorWidth}`},1000);
+//     }
+//     else
+//     {
+//         $(".AllBox").animate({left:"0px"},1000);
+//     }
+
+//end AllBox
+  
+    
+// // start ready screen//
+// $(document).ready(function(){
+//     $(".layerReading").fadeOut(1000,function(){
+//         $("body").css("overflow","auto");
+//     });
+// })
+
+// // end ready screen  //
+
+
+// start scroll
+
+// $(window).scroll(function(){
+//     $(".navbar").removeClass("bg-transparent");
+//     $(".navbar").addClass("bg-white");
+    
+// })
+
+// end scroll
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
